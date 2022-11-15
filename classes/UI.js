fd953cd0963cb47c2c488d28acdf2eb64c6f2c1d
MarkusMaal2 <markus.maal@voco.ee>
2022-11-15 12:00:58 +0200

    Refactor addBook functionality

--- classes/UI.js
+++ classes/UI.js
@@ -1,23 +1,41 @@
 class UI {
+
+    // help function to add DOM element
+
+    addUIElement(name, className = "", textContent = "", attributes = {}) {
+        // create element
+        const element = document.createElement(name)
+
+        // add css
+        if (className !== "") {
+            element.className = className
+        }
+
+        // add textContent
+        element.appendChild(document.createTextNode(textContent))
+
+        // add attributes
+        if (Object.keys(attributes).length > 0) {
+            for (let key in attributes) {
+                element.setAttribute(key, attributes[key])
+            }
+        }
+
+        return element
+    }
+
     addBook(book) {
 
         // create row
-        let newBook = document.createElement("tr")
+        let newBook = this.addUIElement("tr")
 
         // create book columns
-        let colTitle = document.createElement("td")
-        let colAuthor = document.createElement("td")
-        let colISBN = document.createElement("td")
-
-        colTitle.appendChild(document.createTextNode(book.title))
-        colAuthor.appendChild(document.createTextNode(book.author))
-        colISBN.appendChild(document.createTextNode(book.isbn))
+        let colTitle = this.addUIElement("td", "", book.title)
+        let colAuthor = this.addUIElement("td", "", book.author)
+        let colISBN = this.addUIElement("td", "", book.isbn)
 
         // add the delete button to the ISBN column
-        let xLink = document.createElement("a")
-        xLink.href = "#"
-        xLink.className = "secondary-content"
-        xLink.appendChild(document.createTextNode("X"))
+        let xLink = this.addUIElement("a", "secondary-content", "X", {"href": "#"})
         colISBN.appendChild(xLink)
 
         // add all columns to the new row
